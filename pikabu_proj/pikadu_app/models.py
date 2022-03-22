e6be0cc296412444a251352fda98f45709c0fb08
Pavel Anohin <101942009+EvgenyaAnohina@users.noreply.github.com>
2022-03-22 23:44:58 +0300

    created main page

--- pikabu_proj/pikadu_app/models.py
+++ pikabu_proj/pikadu_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import Model
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    slug = models.SlugField(max_length=50, unique=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-date_published']
+
+
+
--- pikabu_proj/pikadu_app/views.py
+++ pikabu_proj/pikadu_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    title = "Последние посты"
+    return render(request, 'pikadu_app/post_list.html', context={'posts':posts,
+                                                                 'title':title})
+
+def post_detail(request, slug_from_request):
+    post = Post.objects.get(slug=slug_from_request)
+    return render(request, 'pikadu_app/post_detail.html', context={'post':post})
