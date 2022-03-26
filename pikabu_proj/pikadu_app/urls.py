996404e5ea36a23dedaa19fe29fedb7c7cd98ee9
Pavel Anohin <101942009+EvgenyaAnohina@users.noreply.github.com>
2022-03-26 11:30:13 +0300

    active post list, tag list and post details

--- pikabu_proj/pikadu_app/urls.py
+++ pikabu_proj/pikadu_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<slug:slug>', views.post_detail, name='post_detail'),
+    path('tags', views.tag_list, name='tag_list_url'),
+    path('<str:slug_from_request>', views.post_detail, name='post_detail_url'),
     path('', views.post_list, name='post_list'),
 ]
--- pikabu_proj/pikadu_app/views.py
+++ pikabu_proj/pikadu_app/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 def post_list(request):
     posts = Post.objects.all()
     title = "Последние посты"
-    return render(request, 'pikadu_app/post_list.html', context={'posts':posts,
-                                                                 'title':title})
+    return render(request, 'pikadu_app/object_list.html',
+                  context={'objects':posts,
+                           'title':title}
+                  )
 
 def post_detail(request, slug_from_request):
     post = Post.objects.get(slug=slug_from_request)
-    return render(request, 'pikadu_app/post_detail.html', context={'post':post})
+    return render(request, 'pikadu_app/post_detail.html',
+                  context={'post':post}
+                  )
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    title = "Список тэгов"
+    return render(request, 'pikadu_app/object_list.html',
+                  context={'objects':tags,
+                           'title':title}
+                  )
