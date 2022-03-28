5d120d18d713d0201b81ee9853a808e72411b6b8
Pavel Anohin <61548239+panohin@users.noreply.github.com>
2022-03-28 09:44:36 +0300

    Delete pikabu_proj directory

--- pikabu_proj/pikadu_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PikaduAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pikadu_app'
--- pikabu_proj/pikadu_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post, Tag
-
-class CreatePostForm(forms.Form):
-    title = forms.CharField()
-    body = forms.Field()
-
-class CreateTagForm(forms.Form):
-    name = forms.CharField()
